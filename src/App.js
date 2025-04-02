92a88f904b93367ba1a5ee6ec59e216741ac9e7e
Prasannadara17 <prasannadara17@gmail.com>
2025-04-02 11:42:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, Lakshmi Prasanna!</h1>
+      <p>Your React app is working! 🚀</p>
+    </div>
+  );
+}
+
+export default App;
